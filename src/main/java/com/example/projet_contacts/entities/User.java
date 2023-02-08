6dde9c97fc245cdadbde5a7cd3f78e1289eee5cc
matthiieu80 <matthieu.comont.pro@outlook.fr>
2023-02-08 14:32:40 +0100

    Create User.java

---
+++ src/main/java/com/example/projet_contacts/entities/User.java
@@ -0,0 +1,87 @@
+package com.example.projet_contacts.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long Id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String img;
+
+    private String motDePasse;
+
+
+    public User() {
+    }
+
+
+    public User(Long id, String firstName, String lastName, String email, String img, String motDePasse) {
+        Id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.img = img;
+        this.motDePasse = motDePasse;
+    }
+
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public String getMotDePasse() {
+        return motDePasse;
+    }
+
+    public void setMotDePasse(String motDePasse) {
+        this.motDePasse = motDePasse;
+    }
+}
