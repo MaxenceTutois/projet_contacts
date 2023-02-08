f9ff1b70030821eb23cfd973532bae5123ec2e84
Maxence <maxence.tutois@outlook.fr>
2023-02-08 13:12:47 +0100

    init

---
+++ src/test/java/com/example/projet_contacts/ProjetContactsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projet_contacts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetContactsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
